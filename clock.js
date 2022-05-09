4ca56c7da20b6628835e3f560bde233e39750e6c
korasin <mchemutai232@gmail.com>
2022-05-09 16:28:47 +0300

    defining setting interval function of the clock

--- clock.js
+++ clock.js
@@ -1,1 +1,10 @@
 // console.log('it loaded') -testing linking
+
+setInterval(setClock,1000)
+
+function setClock(){
+    const currentDate =new Date()
+    const secondRatio = currentDate.getSeconds() /60
+    const minuteRatio = (secondRatio + currentDate.getMinutes()) /60
+    const hourRatio = (secondRatio + currentDate.getHours()) /12
+}
